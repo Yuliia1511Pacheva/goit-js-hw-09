4d7d9b6d1a93a20b41c90b9f71a2ae3e4d919c6b
Yuliia1511Pacheva <juliwoood@gmail.com>
2023-04-16 11:51:10 +0000

    Deploying to gh-pages from @ Yuliia1511Pacheva/goit-js-hw-09@d57b3245a0d9bfab3485143767754dd813feb4d8 🚀

---
+++ 01-color-switcher.e3873058.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){o=setInterval((()=>{n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,t.disabled=!0}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}));let o=null;
+//# sourceMappingURL=01-color-switcher.e3873058.js.map
