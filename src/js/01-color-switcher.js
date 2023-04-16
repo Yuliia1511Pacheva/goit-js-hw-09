d57b3245a0d9bfab3485143767754dd813feb4d8
Yuliia Pacheva <juliwoood@gmail.com>
2023-04-16 14:50:43 +0300

    task1, 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+btnStart.addEventListener('click', onStartClick);
+btnStop.addEventListener('click', onStopClick);
+
+let timerId = null;
+
+function onStartClick() {
+    timerId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor(); 
+        btnStart.disabled = true;
+   },1000) 
+}
+
+function onStopClick(){
+    clearInterval(timerId);
+    btnStart.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,71 @@
+import Notiflix from 'notiflix';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const btnStart = document.querySelector('button[data-start]');
+const days = document.querySelector('span[data-days]');
+const hours = document.querySelector('span[data-hours]');
+const minutes = document.querySelector('span[data-minutes]');
+const seconds = document.querySelector('span[data-seconds]');
+const input = document.querySelector('#datetime-picker');
+
+btnStart.addEventListener('click', onStartClick);
+
+btnStart.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    if (selectedDates[0] < Date.now()) {
+      Notiflix.Notify.info('Please choose a date in the future');
+      btnStart.disabled = true;
+    } else {
+      btnStart.disabled = false;
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+function onStartClick() {
+  const targetDate = new Date(input.value);
+ 
+  const timerId = setInterval(() => {
+    const timeLeft = convertMs(targetDate - Date.now());
+    days.textContent = timeLeft.days.toString().padStart(2, '0');
+    hours.textContent = timeLeft.hours.toString().padStart(2, '0');
+    minutes.textContent = timeLeft.minutes.toString().padStart(2, '0');
+    seconds.textContent = timeLeft.seconds.toString().padStart(2, '0');
+
+    if ((targetDate - Date.now()) <= 0) {
+      clearInterval(timerId);
+      days.textContent = '00';
+      hours.textContent = '00';
+      minutes.textContent = '00';
+      seconds.textContent = '00';
+    }
+  }, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
