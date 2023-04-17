11d49ae53b93535b1ffd517a6ba97d6c6046a7fa
Yuliia Pacheva <juliwoood@gmail.com>
2023-04-17 23:35:52 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import Notiflix, { Notify } from 'notiflix';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() =>{
+
+      if (shouldResolve) {
+    resolve({position, delay})
   } else {
-    // Reject
+    reject ({position, delay})
+  }
+    }, delay)
+  })
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  let { delay: { value: delay }, step: { value: step }, amount: { value: amount } } = evt.currentTarget.elements;
+  
+  delay = Number(delay);
+  step = Number(step);
+  amount = Number(amount);
+
+  for (let i = 1; i <= amount; i += 1){
+    createPromise(i, delay).then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+    })
+    .catch(({ position, delay }) => {
+   Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+    delay += step;
   }
 }
